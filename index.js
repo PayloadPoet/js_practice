25710c21cd1a48c84600180eb44859cb7c0f5db7
PayloadPoet <abdulrafay008877@gmail.com>
2025-07-05 02:25:55 +0500

    feat: add question# 16
    write a javascript function to get the first element of an array.
    Passing a parameter 'n' will return the first 'n' elements of array

--- index.js
+++ index.js
@@ -172,3 +172,15 @@ function typeTeller(arg) {
 }
 console.log(typeTeller([]));
 console.log(typeTeller({}));
+
+// 16. write a javascript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+function retrieve(arr, n = 1) {
+  if (n <= arr.length) {
+    for (let i = 0; i < n; i++) {
+      console.log(arr[i]);
+    }
+  } else {
+    console.log("The provided numbers are too high...");
+  }
+}
+retrieve([1, 2, 3, 4, 5], 3);
