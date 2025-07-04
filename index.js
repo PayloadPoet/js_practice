237a543d25b04a4a125a5bc48421596c1193c426
PayloadPoet <abdulrafay008877@gmail.com>
2025-07-05 00:45:46 +0500

    feat: add 10 question
    get the number of occurences of each letter in specified string

--- index.js
+++ index.js
@@ -73,3 +73,17 @@ function capitalizeFirstLetter(str) {
   console.log(capitalizeWord.join(" "));
 }
 capitalizeFirstLetter("This is my way to doing things.");
+
+// 10. Write a javascript function to get the number of occurrences of each letter in specified string.
+function occ(str) {
+  let occurences = {};
+  str.split("").forEach(function (elem) {
+    if (occurences.hasOwnProperty(elem) === false) {
+      occurences[elem] = 1;
+    } else {
+      occurences[elem]++;
+    }
+  });
+  return occurences;
+}
+console.log(occ("apple"));
